62231887cbe45b7b0c92e0b457deeacbeee443fd
Faizan Qaiser <faizanqaiser9@gmail.com>
2024-01-25 14:24:46 +0500

    PlayerInput Class
    Calculates player's movement and face direction using global input

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private InputManager m_InputManager;
+
+    private GameInput m_GlobalInput;
+
+    private void Start() {
+        m_InputManager = InputManager.Instance;
+        m_GlobalInput = m_InputManager.GlobalInput;
+
+        m_GlobalInput.PlayerMovement.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public Vector2 GetMovementDir() {
+        return m_GlobalInput.PlayerMovement.MovementDir.ReadValue<Vector2>();
+    }
+
+    /// <param name="baseRotDir">This is a vector on the xz plane which defines the direction the player is looking at when its rotation is 0.</param>
+    public float GetFaceDir(Vector3 playerPosition, Camera playerCam, Vector2 baseRotDir) {
+        Vector3 mousePos = m_GlobalInput.PlayerMovement.MousePosition.ReadValue<Vector2>();
+
+        mousePos.z = playerCam.nearClipPlane;
+        Ray ray = playerCam.ScreenPointToRay(mousePos);
+        Plane ground = new(Vector3.up, playerPosition);
+
+        ground.Raycast(ray, out float distance);
+        Vector3 point = ray.GetPoint(distance);
+
+        Vector3 playerToMouse = point - playerPosition;
+
+        return -Vector2.SignedAngle(baseRotDir, new(playerToMouse.x, playerToMouse.z));
+    }
+
+    private void OnDestroy() {
+        m_GlobalInput.PlayerMovement.Disable();
     }
 }
