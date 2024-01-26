552e136249f89b86290fcd1bf9df73a8f62c3bb7
Faizan Qaiser <faizanqaiser9@gmail.com>
2024-01-26 18:14:55 +0500

    Player is now using Rigidbody

--- Assets/Scripts/Player/PlayerMotor.cs
+++ Assets/Scripts/Player/PlayerMotor.cs
@@ -1,23 +1,21 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMotor : MonoBehaviour
 {
     public Vector3 VelocityDir;
     public float Speed;
     public float Rotation;
 
-    private Transform m_Transform;
+    private Rigidbody m_Rb;
 
     private void Start() {
-        m_Transform = transform;
+        m_Rb = GetComponent<Rigidbody>();
     }
 
-    private void Update() {
-        var rot = m_Transform.rotation.eulerAngles;
-        rot.y = Rotation;
-        m_Transform.rotation = Quaternion.Euler(rot);
-
-        m_Transform.position += VelocityDir * Speed * Time.deltaTime;
+    private void FixedUpdate() {
+        m_Rb.rotation = Quaternion.Euler(new(0f, Rotation, 0f));
+        m_Rb.velocity = VelocityDir * Speed;
     }
 
 }
