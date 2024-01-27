3339540786808ffac68f04c777d04663b4ef0025
YanzheWu <wuy420@newschool.edu>
2024-01-27 13:46:56 -0500

    Update UI

--- Assets/Scripts/UI/GameOverWinMenuUI.cs
+++ Assets/Scripts/UI/GameOverWinMenuUI.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameOverWinMenuUI : UIBase
 {
+    [SerializeField] TextMeshProUGUI m_resultText;
+
+    public override void TransitionIntoScreen()
+    {
+        m_resultText.text = $"Congrats! You earned ${OverlayUI.Instance.CurrentMoney} today!";
+        base.TransitionIntoScreen();
+    }
     public void Exit()
     {
         UIManager.Instance.TransitionToScreen(UIScreen.StartMenu);
