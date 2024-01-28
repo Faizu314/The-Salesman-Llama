046bdac6d0bb40a5baec14c53333ca14756770d5
YanzheWu <wuy420@newschool.edu>
2024-01-28 12:01:20 -0500

    Added UI instruction

--- Assets/Scripts/UI/GameOverFailMenuUI.cs
+++ Assets/Scripts/UI/GameOverFailMenuUI.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameOverFailMenuUI : UIBase
 {
+    [SerializeField] TextMeshProUGUI m_resultText;
+
+    public override void TransitionIntoScreen()
+    {
+        m_resultText.text = $"You earned ${OverlayUI.Instance.CurrentMoney} today, but not enough!";
+        base.TransitionIntoScreen();
+    }
+
     public void Exit()
     {
         UIManager.Instance.TransitionToScreen(UIScreen.StartMenu);
