7917d3b7009be33ea19adaa78e7b84758b64d4c4
YanzheWu <wuy420@newschool.edu>
2024-01-27 20:42:44 -0500

    add special attack animation

--- Assets/Scripts/SimpleWeaponSystem/Projectile.cs
+++ Assets/Scripts/SimpleWeaponSystem/Projectile.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
@@ -11,22 +12,29 @@ public class Projectile : MonoBehaviour
     [SerializeField] private ParticleSystem m_HitParticles;
 
     public float CooldownTime => m_ProjectileData.Cooldown;
+    public Sprite Icon => m_ProjectileData.Icon;
 
     private Vector3 m_StartPos;
     private Rigidbody m_Rb;
     private Action<Projectile> m_OnDestroy;
+    private Collider m_collider;
 
     private void Awake()
     {
         m_Rb = GetComponent<Rigidbody>();
+        m_collider = GetComponentInChildren<Collider>();
+        m_collider.enabled = false;
     }
 
     public void Shoot(Vector3 position, Vector3 forward, Action<Projectile> onDestroy)
     {
+        Debug.Log("Shoot " + gameObject.name);
         AudioManager.Instance.PlayOneShot(FModEvents.Instance.NormalAttack, transform.position);
 
+        m_collider.enabled = true;
         m_Rb.isKinematic = false;
 
+        transform.parent = null;
         transform.position = position;
         transform.forward = forward;
 
@@ -43,6 +51,9 @@ public class Projectile : MonoBehaviour
 
     public void Wield(Transform socket)
     {
+        Debug.Log("Wield " + gameObject.name);
+        if (m_Rb == null)
+            m_Rb = GetComponent<Rigidbody>();
         m_Rb.isKinematic = true;
         transform.parent = socket;
         transform.localPosition = m_ProjectileData.LocalPositionOffset;
@@ -51,6 +62,7 @@ public class Projectile : MonoBehaviour
 
     private void OnHit()
     {
+        m_collider.enabled = false;
         StopAllCoroutines();
 
         if (m_HitParticles != null)
--- Assets/Scripts/SimpleWeaponSystem/SpecialThrower.cs
+++ Assets/Scripts/SimpleWeaponSystem/SpecialThrower.cs
@@ -2,66 +2,89 @@
 using UnityEngine;
 using UnityEngine.Pool;
 
-public class SpecialThrower : MonoBehaviour {
+public class SpecialThrower : MonoBehaviour
+{
 
     [SerializeField] private Transform m_SpecialAttackSocket;
     [SerializeField] private Transform m_AttackStartPoint;
-
+    [SerializeField] private Animator m_Animator;
     [SerializeField] private SpecialProjectile m_SpecialProjectilePrefab;
+    [SerializeField] private PlayerAnimationListener m_animationListener;
+    [SerializeField] private SpecialAttackInicatorUI m_indicatorUI;
 
     private ObjectPool<SpecialProjectile> m_SpecialProjectilesPool;
     private SpecialProjectile m_LoadedProjectile;
     private bool m_CanThrow = true;
 
-    private void Start() {
+    private void Start()
+    {
+        m_animationListener.throwProjectile += () => StartCoroutine(nameof(Shoot));
         m_SpecialProjectilesPool = new(CreateProjectile, GetProjectile, ReleaseProjectile, defaultCapacity: 10, maxSize: 10);
-
-        StartCoroutine(nameof(Test_Co));
+        StartCoroutine(nameof(Cooldown_Co), 2);
     }
 
-    private IEnumerator Test_Co() {
+    private IEnumerator Test_Co()
+    {
         yield return new WaitForSeconds(1f);
         LoadSpecialAttack();
     }
 
-    private SpecialProjectile CreateProjectile() {
+    private SpecialProjectile CreateProjectile()
+    {
         var proj = Instantiate(m_SpecialProjectilePrefab);
         return proj;
     }
 
-    private void GetProjectile(SpecialProjectile proj) {
-        proj.gameObject.SetActive(true);
+    private void GetProjectile(SpecialProjectile proj)
+    {
+        proj.Prepare();
+        m_indicatorUI.EnableIcon(proj.ActivatedProjectile.Icon);
     }
 
-    private void ReleaseProjectile(SpecialProjectile proj) {
+    private void ReleaseProjectile(SpecialProjectile proj)
+    {
         proj.gameObject.SetActive(false);
     }
 
-    private void LoadSpecialAttack() {
+    private void PrepareNextSpecialAttack()
+    {
         m_LoadedProjectile = m_SpecialProjectilesPool.Get();
-        m_LoadedProjectile.Wield(m_SpecialAttackSocket);
+        AudioManager.Instance.PlayOneShot(FModEvents.Instance.CoolDownSpecial, transform.position);
     }
 
-    public void SpecialAttack() {
+    private void LoadSpecialAttack()
+    {
+        Debug.Log("Load " + m_LoadedProjectile.ActivatedProjectile.gameObject.name);
+        m_indicatorUI.DisableIcon();
+        if (m_LoadedProjectile == null) m_LoadedProjectile = m_SpecialProjectilesPool.Get();
+        m_LoadedProjectile.Wield(m_SpecialAttackSocket);
+        m_LoadedProjectile.gameObject.SetActive(true);
+        m_Animator.SetBool("Throw", true);
+    }
+
+    public void SpecialAttack()
+    {
         if (!m_CanThrow)
             return;
 
-        float coolDown = m_LoadedProjectile.Shoot(m_AttackStartPoint.position, m_AttackStartPoint.forward, (x) => m_SpecialProjectilesPool.Release(x));
-
-        m_CanThrow = false;
-        StartCoroutine(nameof(Cooldown_Co), coolDown);
+        LoadSpecialAttack();
     }
 
-    private IEnumerator Cooldown_Co(float cooldownTime) {
+    private IEnumerator Shoot()
+    {
+        float coolDown = m_LoadedProjectile.Shoot(m_AttackStartPoint.position, m_AttackStartPoint.forward, (x) => m_SpecialProjectilesPool.Release(x));
+
+        m_Animator.SetBool("Throw", false);
+        m_CanThrow = false;
+        yield return Cooldown_Co(coolDown);
+    }
+
+    private IEnumerator Cooldown_Co(float cooldownTime)
+    {
         yield return new WaitForSeconds(cooldownTime);
 
         m_CanThrow = true;
 
-        OnCooldown();
-    }
-
-    private void OnCooldown() {
-        AudioManager.Instance.PlayOneShot(FModEvents.Instance.CoolDownSpecial, transform.position);
-        LoadSpecialAttack();
+        PrepareNextSpecialAttack();
     }
 }
